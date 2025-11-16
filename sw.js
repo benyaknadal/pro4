803726e599d15d1e5a0d9aec6f67bca9ed739152
Nadal Benyak <benyakhelf.mohamednidhal@fs.univ-medea.dz>
2025-11-16 17:15:12 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,1 @@
-const CACHE = "v1";
-const ASSETS = [
-  "index.html",
-  "manifest.json",
-  "logo1.png"
-];
-
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-});
-
-self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
-
+const CACHE='v1';const ASSETS=['index.html','manifest.json','logo1.png'];self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
